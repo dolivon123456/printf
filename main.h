4b3591036709689c2af00d00a6b4b043a4846d95
Sharon and josephine <dolivon5555@gmail.com>
2022-08-07 10:11:51 -0700

    printf

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define DECIMAL 10
+#define HEX 16
+#define OCTAL 8
+#define BINARY 2
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
