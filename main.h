cd55a38fa7bd564128ece90220ac8a8055ce3a44
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-08-08 09:26:12 +0100

    Update main.h

--- main.h
+++ main.h
@@ -10,7 +10,43 @@
 #define OCTAL 8
 #define BINARY 2
 
+/**
+ * struct specifierStruct - struct to hold all specifier
+ * @specifier: character pointer
+ * @func: callback funtion
+ */
+typedef struct specifierStruct
+{
+	char *specifier;
+	int (*func)(const char *, int, va_list, int *);
+} specifierStruct;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int check_specifier(const char *, int, va_list, int *);
+int isDigit(char);
+int isAlpha(char);
+int setFlags(const char *, int *, int, va_list);
+int print_string(char *, int *, int);
+char *rot13(char *s);
+int get_index(char *, char);
+int _vprintf(const char *, va_list);
+int _printf(const char *, ...);
+void signedNumberToString(int64_t, int, char *, const char *, int);
+void unsignedNumberToString(uint64_t, int, char *, const char *, int);
+int _putchar(char);
+int format_specifier(int, const char *, va_list, int *, int *,  int *, int *);
+char *reversed(char *);
+int print_char(const char *, int, va_list, int *);
+int print_int(const char *, int, va_list, int *);
+int print_str(const char *, int, va_list, int *);
+int print_unsigned(const char *, int, va_list, int *);
+int print_addr(const char *, int, va_list, int *);
+int print_oct_bin(const char *, int, va_list, int *);
+int print_rot13(const char *, int, va_list, int *);
+int print_reverse(const char *, int, va_list, int *);
+char *upper_str(char *);
+int isPrintable(char);
+int upper_strlen(char *);
 
 #endif
